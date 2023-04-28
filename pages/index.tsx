a3fe141ebe7627415fcf871302c6cf806cda2090
shenda <shenda@hqshuke.com>
2023-04-28 15:16:18 +0800

    feat: INIT with react + ts + antd

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
 
+import { Button } from 'antd';
+
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
     <p>
       <Link href="/about">About</Link>
     </p>
+    <div>
+      <Button type="primary">Hello World!</Button>
+    </div>
   </Layout>
 )
 
