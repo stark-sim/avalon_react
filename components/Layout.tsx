bb02cf13f990a946361026b5e06fac483f0aa650
shenda <shenda@hqshuke.com>
2023-05-08 09:58:32 +0800

    feat: add 备案图标

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,13 @@
-import React, { ReactNode } from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+import React, { ReactNode } from "react";
+import Link from "next/link";
+import Head from "next/head";
 
 type Props = {
-  children?: ReactNode
-  title?: string
-}
+  children?: ReactNode;
+  title?: string;
+};
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
+const Layout = ({ children, title = "This is the default title" }: Props) => (
   <div>
     <Head>
       <title>{title}</title>
@@ -16,18 +16,63 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
     </Head>
     <header>
       <nav>
-        <Link href="/">Home</Link> | <Link href="/about">About</Link> |{' '}
-        <Link href="/users">Users List</Link> |{' '}
+        <Link href="/">Home</Link> | <Link href="/about">About</Link> |{" "}
+        <Link href="/users">Users List</Link> |{" "}
         <a href="/api/users">Users API</a>
       </nav>
     </header>
     {children}
     <footer>
       <hr />
-      <span>I'm here to stay (Footer)</span>
-      <a href="https://beian.miit.gov.cn/" target="_blank">粤ICP备2023039885号</a>
+      {/* <span>I'm here to stay (Footer)</span> */}
+      <div
+        style={{
+          width: "600px",
+          margin: "0 auto",
+          padding: "20px 0",
+          flexDirection: "row",
+          display: "flex",
+        }}
+      >
+        <a href="https://beian.miit.gov.cn/" target="_blank">
+          <p
+            style={{
+              float: "left",
+              height: "20px",
+              lineHeight: "20px",
+              margin: "0px 10px 0px 10px",
+              color: "#939393",
+            }}
+          >
+            粤ICP备2023039885号
+          </p>
+        </a>
+        <a
+          target="_blank"
+          href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44030502009977"
+          style={{
+            display: "inline-block",
+            textDecoration: "none",
+            height: "20px",
+            lineHeight: "20px",
+          }}
+        >
+          <img src="/备案图标.png" style={{ float: "left" }} />
+          <p
+            style={{
+              float: "left",
+              height: "20px",
+              lineHeight: "20px",
+              margin: "0px 0px 0px 5px",
+              color: "#939393",
+            }}
+          >
+            粤公网安备 44030502009977号
+          </p>
+        </a>
+      </div>
     </footer>
   </div>
-)
+);
 
-export default Layout
+export default Layout;
